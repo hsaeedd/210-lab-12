3665f4db9c248b1a6b833468dfea060e92aa6022
Hannah Saeed <hannahsaeed@Hannahs-MBP.attlocal.net>
2025-09-21 22:33:24 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
-//Hannah Saeed
+//COMSC-210 | Lab 12 | Hannah Saeed
 //IDE: VS Code
 
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <numeric>
+#include <array>
 using namespace std;
 
 int main(){
-    
+
     return 0;
 }
